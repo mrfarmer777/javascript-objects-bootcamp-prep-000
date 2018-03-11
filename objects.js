0b6ede17c6f309de18387a90fca9a7ad98257f53
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-11 20:29:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,8 @@ var updatePlaylist= function(obj, artist,title){
   return Object.assign({},obj,{[artist]:title});
 };
 
-var removeFromPlaylist=function(obj,artist){
-  console.log(obj.artist);
+var removeFromPlaylist=function(obj,artistName){
+  var artist = artistName;
   delete obj.artist;
   return obj;
 };
