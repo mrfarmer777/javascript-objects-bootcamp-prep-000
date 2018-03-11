a817609f8916f5afcbb06fa5320a581defabbc58
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-11 20:23:42 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist ={EttaJames:"At last"};
+
+var updatePlaylist= function(obj, artist,title){
+  return Object.assign({},obj,{[artist]:title});
+};
+
+var removeFromPlaylist=function(obj,artist){
+  delete obj.artist;
+  return obj;
+};
